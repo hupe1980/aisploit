aa9be11c62458e0c256228b983f2064c622d1338
hupe1980 <frankhuebner1980@gmail.com>
2024-04-24 23:14:51 +0200

    Fix linter

--- aisploit/classifiers/amazon/comprehend.py
+++ aisploit/classifiers/amazon/comprehend.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Any, Dict, Generic, List, TypeVar, Optional, Callable
+from typing import Any, Callable, Dict, Generic, List, Optional, TypeVar
 
 import boto3
 
